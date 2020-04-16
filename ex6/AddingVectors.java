d4906144a7afe980478a0309efbde01fab35c2a5
Ada Cienciala <adacien@o2.pl>
2020-04-16 13:14:58 +0200

    Renaming files pt2

--- ex6/AddingVectors.java
+++ ex6/AddingVectors.java
@@ -4,88 +4,72 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-class DifferentLengthVectors extends Exception 
-{
+class DifferentLengthVectors extends Exception {
     private int length1;
     private int length2;
 
-    public DifferentLengthVectors(ArrayList<Integer> array1, ArrayList<Integer> array2) 
-    {
+    public DifferentLengthVectors(ArrayList<Integer> array1, ArrayList<Integer> array2) {
         length1 = array1.size();
         length2 = array2.size();
     }
 
-    public void command() 
-    {
+    public void command() {
         System.out.println("One's length is " + length1 + ", and the other's is " + length2 + ".");
     }
 }
 
-public class AddingVectors 
-{
-    public static void main(String[] args) 
-    {
+public class AddingVectors {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Integer> result = new ArrayList<>();
 
-        while (true) 
-        {
+        while (true) {
             System.out.println("Gimmie 2 VALID vectors, please:");
             ArrayList<Integer> v1 = new ArrayList<>();
             ArrayList<Integer> v2 = new ArrayList<>();
 
-            for (String number : sc.nextLine().split(" "))
-            {
+            for (String number : sc.nextLine().split(" ")) {
                 if (number.matches("^-?\\d+"))
                     v1.add(Integer.parseInt(number));
             }
-            for (String number : sc.nextLine().split(" ")) 
-            {
+            for (String number : sc.nextLine().split(" ")) {
                 if (number.matches("^-?\\d+"))
                     v2.add(Integer.parseInt(number));
             }
 
-            try 
-            {
+            try {
                 result = addVectors(v1, v2);
                 break;
             } 
-            catch (DifferentLengthVectors ex) 
-            {
+            catch (DifferentLengthVectors ex) {
                 ex.command();
             }
         }
         saveVectorToFile(result);
     }
 
-    public static void saveVectorToFile(ArrayList<Integer> vector) 
-    {
-        try 
-        {
+    public static void saveVectorToFile(ArrayList<Integer> vector) {
+        try {
             BufferedWriter buffer = new BufferedWriter(new FileWriter("saved_vector.txt"));
-            for (Integer number : vector) 
-            {
+            for (Integer number : vector)
                 buffer.write(number.toString() + " ");
-            }
             buffer.close();
         }
-        catch (IOException ex) 
+        catch (IOException ex)
         {
             System.out.println("Couldn't save that vector to file, sorry!");
         }
-        System.out.println("Saved that bad boi to file!");
+        System.out.println("Saved that bad boi vector to file!");
     }
 
-    public static ArrayList<Integer> addVectors(ArrayList<Integer> v1, ArrayList<Integer> v2) throws DifferentLengthVectors 
-    {
+    public static ArrayList<Integer> addVectors(ArrayList<Integer> v1, ArrayList<Integer> v2)
+            throws DifferentLengthVectors {
         if (v1.size() != v2.size())
             throw new DifferentLengthVectors(v1, v2);
 
         ArrayList<Integer> result = new ArrayList<>();
-        for (int i = 0; i < v1.size(); ++i) 
-        {
+        for (int i = 0; i < v1.size(); ++i)
             result.add(v1.get(i) + v2.get(i));
-        }
 
         return result;
     }
